30ee65d511663e8aa1d8cef5000036c0db20f97e
Liam McArdle <mcardle.liam@gmail.com>
2018-08-27 00:08:50 -0700

    refactor to move todo sections into individual components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,17 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import TodoHeader from './components/TodoHeader';
+import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter'
+
 class App extends Component {
   render() {
     return (
       <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <input className="new-todo" placeholder="What needs to be done?" autofocus />
-        </header>
-
-        <section className="main">
-          <input id="toggle-all" className="toggle-all" type="checkbox" />
-          <label for="toggle-all">Mark all as complete</label>
-          <ul className="todo-list">
-            <li className="completed">
-              <div className="view">
-                <input className="toggle" type="checkbox" checked />
-                <label>Taste JavaScript</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Create a TodoMVC template" />
-            </li>
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>Buy a unicorn</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Rule the web" />
-            </li>
-          </ul>
-        </section>
-          <footer class="footer">
-          <span class="todo-count"><strong>0</strong> item left</span>
-          <ul class="filters">
-            <li>
-              <a class="selected" href="#/">All</a>
-            </li>
-            <li>
-              <a href="#/active">Active</a>
-            </li>
-            <li>
-              <a href="#/completed">Completed</a>
-            </li>
-          </ul>
-          <button class="clear-completed">Clear completed</button>
-        </footer>
+        <TodoHeader />
+        <TodoList />
+        <TodoFooter />
       </section>
     );
   }
---
+++ src/components/TodoFooter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class TodoFooter extends Component {
+  render() {
+    return (
+      <footer class="footer">
+        <span class="todo-count"><strong>0</strong> item left</span>
+        <ul class="filters">
+          <li>
+            <a class="selected" href="#/">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button class="clear-completed">Clear completed</button>
+      </footer>
+    );
+  }
+}
